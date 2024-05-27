d69da32dfe57e68e345a40346129a6170a731a1b
ELCANET\phav <vuong.phamminh@elca.vn>
2024-05-27 23:13:50 +0700

    remove debug code

--- datasets/coco.py
+++ datasets/coco.py
@@ -21,7 +21,6 @@ class CocoDetection(torchvision.datasets.CocoDetection):
         self.prepare = ConvertCocoPolysToMask(return_masks)
 
     def __getitem__(self, idx):
-        print("CocoDetection __getitem__ idx = ", idx)
         img, target = super(CocoDetection, self).__getitem__(idx)
         image_id = self.ids[idx]
         target = {'image_id': image_id, 'annotations': target}
